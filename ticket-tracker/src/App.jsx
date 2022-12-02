5d637d40f4fc7d3df426df60cce4c50aa12f99d9
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2022-12-02 16:40:56 +0000

    Sorting out file structure

---
+++ ticket-tracker/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='App__heading'>
+            <h1>Ticket Tracker</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
